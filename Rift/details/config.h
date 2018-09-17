0e1c2fdc4836e6896202f077ac68ae9fe4d0c180
Ned Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-09-17 14:57:32 -0400

    Added comments

--- Rift/details/config.h
+++ Rift/details/config.h
@@ -3,7 +3,9 @@
 #include <bitset>
 
 namespace rift {
+	// Defines rift's configuration information
 	namespace config {
+		// If more component types are needed modify this value
 		const std::size_t MAX_COMPONENT_TYPES = 64;
 	}
 	using ComponentMask = std::bitset<config::MAX_COMPONENT_TYPES>;
