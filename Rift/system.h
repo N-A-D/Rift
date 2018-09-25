54c25d1305876704544b8a7d42cda045a6423db3
Ned D <ned.austin.datiles@ryerson.ca>
2018-09-25 15:14:06 -0400

    Update system.h

--- Rift/system.h
+++ Rift/system.h
@@ -52,7 +52,7 @@ namespace rift {
 		// Note: Only one system per system type can be managed by any one system 
 		// manager. If a given system manager already manages a system of type S, 
 		// then if another or the same caller were to add another system of the 
-		// same type as S, then that system will replace the currently managed system
+		// same type S, then that system will replace the currently managed system
 		// example:
 		// SystemManager sys_mgr(entity_manager);
 		// sys_mgr.add<MovementSystem>();
