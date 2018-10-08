f39cbc22641c8f779d889881188d8c902838a6d4
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-10-08 16:56:37 -0400

    Renamed & moved the signature generator function into functions.h

---
+++ Rift/details/functions.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "config.h"
+
+namespace rift {
+
+	// Given a template parameter pack of Component types, this function returns the ComponentMask for those types
+	// example: ComponentMask mask = mask_for<Position, Velocity, Direction>();
+	template <class ...Components>
+	ComponentMask signature_for() noexcept {
+		ComponentMask mask = 0;
+		[&mask](...) {}((mask.set(Components::family()))...);
+		return mask;
+	}
+
+}
