3358aa865cd873dd229d61bf8bbcb9093983e713
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-10-08 17:01:32 -0400

    Updated comment

--- Rift/details/functions.h
+++ Rift/details/functions.h
@@ -5,7 +5,7 @@
 namespace rift {
 
 	// Given a template parameter pack of Component types, this function returns the ComponentMask for those types
-	// example: ComponentMask mask = mask_for<Position, Velocity, Direction>();
+	// example: ComponentMask mask = signature_for<Position, Velocity, Direction>();
 	template <class ...Components>
 	ComponentMask signature_for() noexcept {
 		ComponentMask mask = 0;
