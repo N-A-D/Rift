f47e3d0fe16c31d49a04cf62bba3bf5dc1909970
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-10-11 22:10:49 -0400

    The Pool class is now the Cache class.

--- Rift/utility/containers.h
+++ Rift/utility/containers.h
@@ -4,203 +4,172 @@
 
 namespace rift {
 
-	// The Base Component Pool
-	class BasePool {
+	// The BaseCache class
+	// An interface that specifies what 'cache' objects should be able to do
+	class BaseCache {
 	public:
 		
-		using size_type = std::size_t;
+		BaseCache() = default;
+		virtual ~BaseCache() = default;
 
-		virtual ~BasePool() {}
-		// Return a pointer to the component mapped to index
-		// If the index is not valid, then returns nullptr
-		virtual void* get(std::size_t index) = 0;
-		// Inserts a component at the given index
-		// If the pool is at capacity, then the pool will expand 
-		virtual void insert(size_type index, void* object) = 0;
-		// Removes the component at the given index
-		virtual void remove(size_type index) = 0;
-		// Clears the component pool
+		virtual bool empty() const noexcept = 0;
 		virtual void clear() noexcept = 0;
-		// Returns the number of components in the pool
-		virtual size_type size() const noexcept = 0;
-		// Returns the number of components that can fit into the pool currently
-		virtual size_type capacity() const noexcept = 0;
+		virtual std::size_t size() const noexcept = 0;
+		virtual std::size_t capacity() const noexcept = 0;
+		virtual bool test(std::size_t index) const noexcept = 0;
+		virtual void insert(std::size_t index, void* object) = 0;
+		virtual void erase(std::size_t index) = 0;
+		virtual void* get(std::size_t index) = 0;
+
 	};
-
-	// The Component Pool class
-	// Manages a contiguous block of components
-	template <class Component>
-	class Pool : public BasePool {
+	
+	// The Cache class
+	// Provides contiguous storage for objects of a single type
+	// Note:
+	// - Caches cannot be sorted
+	// - Caches are read only when iterating.
+	// - The class is implemented in a very similar way to a SparseSet
+	//   *Clearing the cache takes O(n)
+	template <class T>
+	class Cache : public BaseCache {
 	public:
+	
+		template <bool is_const>
+		class Iterator final {
+		public:
 
-		using value_type = Component;
-		using reference = Component &;
-		using const_reference = const Component&;
-		using iterator = typename std::vector<Component>::iterator;
-		using const_iterator = typename std::vector<Component>::const_iterator;
+			using value_type = T;
+			using difference_type = std::ptrdiff_t;
+			using reference = typename std::conditional_t<is_const, const T&, T&>;
+			using pointer = typename std::conditional_t<is_const, const T*, T*>;
+			using iterator_category = std::input_iterator_tag;
 
-		Pool() {}
-		virtual ~Pool() {}
+			Iterator() = default;
+			Iterator(const Iterator&) = default;
+			Iterator& operator=(const Iterator&) = default;
+			
+			explicit Iterator(T* data)
+				: current(data) {}
 
-		/// Iterators to the packed set of components
-		iterator begin() { return components.begin(); }
-		iterator end() { return components.end(); }
-		const_iterator begin() const { return components.begin(); }
-		const_iterator end() const { return components.end(); }
-		
-		// Returns a reference to the component at a given index
-		inline reference at(size_type index) { return *(static_cast<Component *>(get(index))); }
+			template <bool _cond = is_const>
+			std::enable_if_t<_cond, reference>
+			operator*() const {
+				return *current;
+			}
 
-		// Returns a const reference to the component at a given index
-		inline const_reference at(size_type index) const { return *(static_cast<Component *>(get(index))); }
+			template <bool _cond = is_const>
+			std::enable_if_t<!_cond, reference>
+			operator*() const {
+				return *current;
+			}
 
-		// Returns a pointer to the memory of the Component
-		// Note:
-		// - Do not delete the pointer!
-		inline void* get(std::size_t index) override {
-			assert(forward.at(index) < components.size());
-			return /*(void *)*/(&components.at(forward.at(index)));
-		}
+			Iterator& operator++() {
+				++current;
+				return *this;
+			}
+			
+			Iterator& operator++(int) {
+				Iterator tmp = *this;
+				++current;
+				return tmp;
+			}
 
+			bool operator==(const Iterator& other) const {
+				return current == other.current;
+			}
 
-		// Inserts a component into the Pool overwritting any pre-existing component at the given index
-		// Note:
-		// - Since instances of this class will be managed by an EntityManager, the only components that will be
-		//   present in the pool are those that are add-ed by live entities.
-		// - Entities are only able to add a given component type once so there is no worry about multiple insertions
-		inline void insert(size_type index, void* object) override {
-			// If the pool isn't large enough to fit a component at the given index, 
-			// expand the pool so that it is.
-			if (forward.size() <= index)
-				forward.resize(index + 1);
+			bool operator!=(const Iterator& other) const {
+				return !(*this == other);
+			}
 
-			// Store the location TO the new component
-			forward.at(index) = components.size();
+		private:
+			T* current;
+		};
 
-			// Store the location FROM the component to the location of the index TO the component
-			backward.push_back(index);
+		using size_type = std::size_t;
+		using value_type = T;
+		using reference = T&;
+		using const_reference = const T&;
+		using iterator = Iterator<false>;
+		using const_iterator = Iterator<true>;
 
-			// Add the component to the set
-			components.push_back(*(static_cast<Component *>(object)));
-		}
+		Cache() = default;
+		virtual ~Cache() = default;
 
-		// Remove any component at the given index
-		// Note:
-		// - While is is possible to continuously delete a component at a single index, an Entity can only ever
-		//   delete a component of a given type from itself once. It if wishes to delete the component again, it
-		//   must first re-add the component then remove it. Since there are external control mechanisms governing
-		//   the ability to remove components, there is no need to worry about repeated deletion at a given index
-		inline void remove(size_type index) override {
-			assert(index < forward.size() && 
-				   forward.at(index) < components.size() && 
-				   "There is no component at the given index!");
+		iterator begin() { return Iterator<false>(instances.data()); }
+		iterator end() { return Iterator<false>(instances.data() + instances.size()); }
+		const_iterator begin() const { return Iterator<true>(instances.data()); }
+		const_iterator end() const { return Iterator<true>(instances.data() + instances.size()); }
 
-			// Swap the component to be destroyed with the component at the back of the set
-			components.at(forward.at(index)) = components.back();
-
-			// Update the mapping from TO the last component's location
-			forward.at(backward.back()) = forward.at(index);
-
-			// Update the mapping FROM the last component's location TO the location of the mapping in the reverse direction
-			backward.at(forward.at(index)) = backward.back();
-
-			// Remove the last component as it has been moved
-			components.pop_back();
-			backward.pop_back();
-		}
-
-		// Clears the pool of any components
-		void clear() noexcept override {
-			components.clear();
-			backward.clear();
-		}
-
-		// Return the number of components that are currently in the pool
-		size_type size() const noexcept override { return components.size(); }
-
-		// Returns the current number of Components that the Pool can support
+		bool empty() const noexcept override { return reverse.empty(); }
+		void clear() noexcept override { instances.clear(); reverse.clear(); }
+		size_type size() const noexcept override { return reverse.size(); }
 		size_type capacity() const noexcept override { return forward.size(); }
 
-	private:
+		// Check if there exists an object at the given index
+		bool test(size_type index) const noexcept override;
 
-		// The collection of components
-		std::vector<Component> components;
-		// Collection of indexes to components
+		// Insert an object at the given index
+		// Note:
+		// - Multiple insertions at the same index is not permitted
+		void insert(size_type index, void* object) override;
+		// Remove an object at the given index
+		// Notes:
+		// - An assertion is made that there is an object at the given index
+		// - Multiple removals at the same index are not permitted
+		void erase(size_type index) override;
+
+		// Return a opaque pointer to the object at the given index
+		// Note:
+		// - An assertion is made that there exists an object at the given index
+		void* get(size_type index) override;
+
+	private:
+		// The collection of object instances
+		std::vector<T> instances;
+		// Collection of indexes to instance objects
 		std::vector<size_type> forward;
-		// Collectino of indexes from components
-		std::vector<size_type> backward;
+		// Collection of active indexes in forward
+		std::vector<size_type> reverse;
 	};
 
-	// The ResultSet class
-	// Is, and can only be, used for storing rift::Entity's in compact form for use in entities_with<Components...> queries
-	template <class Entity>
-	class ResultSet {
-	public:
-
-		using size_type = std::size_t;
-		using iterator = typename std::vector<Entity>::iterator;
-		using const_iterator = typename std::vector<Entity>::const_iterator;
-
-		ResultSet() = default;
-
-		// Iterators into the set
-
-		iterator begin() { return entities.begin(); }
-		iterator end() { return entities.end(); }
-		const_iterator begin() const { return entities.begin(); }
-		const_iterator end() const { return entities.end(); }
-
-		// Checks if the ResultSet already contains a given entity
-		inline bool has(const Entity& e) const {
-			auto idx = e.id().index();
-			if (idx >= forward.size())
-				return false;
-			if (forward.at(idx) < entities.size() && 
-				entities.at(forward.at(idx)) == e)
-				return true;
+	template<class T>
+	inline bool Cache<T>::test(size_type index) const noexcept
+	{
+		if (index >= forward.size())
 			return false;
-		}
+		if (forward.at(index) < reverse.size() && reverse.at(forward.at(index)) == index)
+			return true;
+		return false;
+	}
 
-		// Inserts an entity into the result set 
-		// Notes:
-		// - An assertion is made that the entity does not exist in the set
-		// - If unsure as to whether or not the entity exists, first check
-		//   for the entity using the has() function
-		// - If the entity's index is greater than the size of the set, the 
-		//   set will expand to accommodate the entity
-		inline void insert(const Entity& e) {
-			assert(!has(e));
-			auto idx = e.id().index();
-			if (idx >= forward.size())
-				forward.resize(idx + 1);
-			forward.at(idx) = entities.size();
-			entities.push_back(e);
-		}
+	template<class T>
+	inline void Cache<T>::insert(size_type index, void * object)
+	{
+		assert(!test(index));
+		if (index >= forward.size())
+			forward.resize(index + 1);
+		forward.at(index) = reverse.size();
+		instances.push_back(*(static_cast<T *>(object)));
+		reverse.push_back(index);
+	}
 
-		// Removes an entity from the result set
-		// Notes:
-		// - An assertion is made that the entity exists within the set
-		// - If unsure as to whether or not the entity exists, first check
-		//   for the entity using the has() function
-		inline void remove(const Entity& e) {
-			assert(has(e));
-			auto idx = e.id().index();
-			auto sub = entities.back();
-			entities.at(forward.at(idx)) = sub;
-			forward.at(sub.id().index()) = forward.at(idx);
-			entities.pop_back();
-		}
+	template<class T>
+	inline void Cache<T>::erase(size_type index)
+	{
+		assert(test(index));
+		instances.at(forward.at(index)) = instances.back();
+		reverse.at(forward.at(index)) = reverse.back();
+		forward.at(reverse.back()) = forward.at(index);
+		instances.pop_back();
+		reverse.pop_back();
+	}
 
-		// Returns the number of entity's in the set
-		size_type size() const noexcept { return entities.size(); }
-
-		// Returns the capacity of the set. I.e., how many entities it can possibly hold
-		size_type capacity() const noexcept { return forward.size(); }
-
-	private:
-
-		std::vector<Entity> entities;
-		std::vector<std::size_t> forward;
-	};
+	template<class T>
+	inline void * Cache<T>::get(size_type index)
+	{
+		assert(test(index));
+		return (void*)&instances.at(forward.at(index));
+	}
 
 }
