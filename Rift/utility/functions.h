bbca2a5d7f32965ea33e48d44fd5f0b7cfa2230a
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-10-19 14:04:48 -0400

    Added comments

--- Rift/utility/functions.h
+++ Rift/utility/functions.h
@@ -6,7 +6,10 @@
 namespace rift {
 
 	// Given a template parameter pack of Component types, this function returns the ComponentMask for those types
-	// example: ComponentMask mask = signature_for<Position, Velocity, Direction>();
+	// example: ComponentMask mask = signature_for<Position, Direction>();
+	// Note:
+	// - Ordering of the types does not matter, the function will still return the same component mask. That is, 
+	//   signature_for<Position, Direction>() == signature_for<Direction, Position>()
 	template <class ...Components>
 	ComponentMask signature_for() noexcept {
 		ComponentMask mask;
