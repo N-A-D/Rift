45331dd672a9b3e8fbb8fecb898d7fad25d86dee
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-10-20 20:51:28 -0400

    Set the maximum # of components to 128 instead of 64

--- Rift/config.h
+++ Rift/config.h
@@ -7,7 +7,7 @@ namespace rift {
 	// Defines rift's configuration information
 	namespace config {
 		// If more component types are needed modify this value
-		const std::size_t MAX_COMPONENT_TYPES = 64;
+		const std::size_t MAX_COMPONENT_TYPES = 128;
 	}
 	using ComponentMask = std::bitset<config::MAX_COMPONENT_TYPES>;
 }
