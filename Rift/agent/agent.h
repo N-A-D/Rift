7d81db244d453b04d7d7e6f1934fcb741c8d0d87
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-11-02 13:47:12 -0400

    Added 2x2 matrix class and the CellSpace class

--- Rift/agent/agent.h
+++ Rift/agent/agent.h
@@ -6,30 +6,32 @@
 #include <cmath>
 #include "../rift.h"
 #include <stdexcept>
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+#include <list>
 #include <array>
 
 namespace rift {
 
-	// Implementation namespace - not to be used by clients
-	namespace math {
-
-		// 2D vector for positions and directions
+	// namespace is not to be used
+	namespace impl {
+		
 		template <class T>
 		struct Vec2 {
-			Vec2()
-				: x(0), y(0) {}
-			Vec2(T x, y)
+			Vec2() : x(0), y(0) {}
+			Vec2(T x, T y)
 				: x(x), y(y) {}
 
 			Vec2<T>& operator+=(const Vec2<T>& v) noexcept {
 				x += v.x;
-				y += y.x;
+				y += v.y;
 				return *this;
 			}
 
 			Vec2<T>& operator-=(const Vec2<T>& v) noexcept {
 				x -= v.x;
-				y -= y.x;
+				y -= v.y;
 				return *this;
 			}
 
@@ -39,142 +41,341 @@ namespace rift {
 				return *this;
 			}
 
-			Vec2<T>& operator/=(T scale) {
-				if (scale == 0.0) throw std::runtime_error("Division by zero!");
-				x /= scale;
-				y /= scale;
-				return *this;
-			}
-
 			T x, y;
 		};
-		
-		// Converts degrees to radians
-		template <class T>
-		T radians(T degrees) noexcept {
-			return degrees * static_cast<T>(M_PI) / static_cast<T>(180.0);
-		}
+
+		template <class T> Vec2<T> operator+(const Vec2<T>& u, const Vec2<T>& v) noexcept { return Vec2<T>(u) += v; }
+		template <class T> Vec2<T> operator-(const Vec2<T>& u, const Vec2<T>& v) noexcept { return Vec2<T>(u) -= v; }
+		template <class T> Vec2<T> operator*(const Vec2<T>& u, T scale) noexcept { return Vec2<T>(u) *= scale; }
+		template <class T> Vec2<T> operator*(T scale, const Vec2<T>& u) noexcept { return u * scale; }
+
+		using Vec2f = Vec2<float>;
+		using Vec2d = Vec2<double>;
 
 		// Converts radians to degrees
 		template <class T>
-		T degrees(T radians) noexcept {
-			return radians * static_cast<T>(180.0) / static_cast<T>(M_PI);
-		}
+		T degrees(T radians) noexcept;
 
-		// Returns the length of a vector u
+		// Converts degrees to radians 
 		template <class T>
-		T length(const Vec2<T>& u) noexcept {
-			return sqrt(pow(u.x, 2) + pow(u.y, 2));
-		}
+		T radians(T degrees) noexcept;
 
-		// Returns the length squared of a vector u
+		// Returns the length of u
 		template <class T>
-		T length_sq(const Vec2<T>& u) noexcept {
-			return pow(u.x, 2) + pow(u.y, 2);
-		}
+		T length(const Vec2<T>& u) noexcept;
 
-		// Returns the distance between vectors u and v
+		// Returns the length squared of u
 		template <class T>
-		T dist_btwn(const Vec2<T>& u, const Vec2<T>& v) noexcept {
-			return sqrt(pow(v.x - u.x, 2) + pow(v.y - u.y, 2));
-		}
+		T length_sq(const Vec2<T>& u) noexcept;
 
-		// Returns the distance squared between vectors u and v
+		// Returns the distance between u and v
 		template <class T>
-		T dist_btwn_sq(const Vec2<T>& u, const Vec2<T>& v) noexcept {
-			return pow(v.x - u.x, 2) + pow(v.y - u.y, 2);
-		}
+		T dist_btwn(const Vec2<T>& u, const Vec2<T>& v) noexcept;
 
-		// Returns the dot product between u and v
+		// Returns the distance squared between u and v
 		template <class T>
-		T dot(const Vec2<T>& u, const Vec2<T>& v) noexcept {
-			return u.x * v.x + u.y * v.y;
-		}
+		T dist_btwn_sq(const Vec2<T>& u, const Vec2<T>& v) noexcept;
 
-		// Returns a rotated u by theta degrees
+		// Returns the angle between u and v
 		template <class T>
-		Vec2<T> rotate(const Vec2<T>& u, T theta) noexcept {
-			auto rads = radians(theta);
-			auto x = u.x * cos(rads) - u.y * sin(rads);
-			auto y = u.x * sin(rads) + u.y * cos(rads);
-			return Vec2<T>(x, y);
-		}
+		T angle_btwn(const Vec2<T>& u, const Vec2<T>& v) noexcept;
 
-		// Returns a normalized vector u
+		// Returns the dot product of u and v
 		template <class T>
-		Vec2<T> norm(const Vec2<T>& u) {
-			auto mag = length(u);
-			if (mag == 0) throw std::runtime_error("Cannot normalize the zero vector!");
-			return Vec2<T>(u.x / mag, u.y / mag);
-		}
+		T dot(const Vec2<T>& u, const Vec2<T>& v) noexcept;
 
-		// Returns the angle in degrees between u and v
+		// Returns a normalized u
 		template <class T>
-		T angle_between(const Vec2<T>& u, const Vec2<T>& v) {
-			return degrees(acos(dot(norm(u), norm(v)));
-		}
+		Vec2<T> norm(const Vec2<T>& u) noexcept;
 
-		// Truncates the length of the vector u to length
+		// Returns a truncated to 'length' u
 		template <class T>
-		Vec2<T> trunc(const Vec2<T>& u, T length) {
-			auto v = norm(u);
-			v *= size;
-			return v;
-		}
+		Vec2<T> trunc(const Vec2<T>& u, T length) noexcept;
+
+		// Returns a rotated u by 'angle' degrees
+		template <class T>
+		Vec2<T> rotate(const Vec2<T>& u, T angle) noexcept;
 
 		// Returns a vector orthogonal to u
 		// Note:
 		// - The vector is +90 degrees from u
 		template <class T>
-		Vec2<T> ortho(const Vec2<T>& u) noexcept {
+		Vec2<T> ortho(const Vec2<T>& u) noexcept;
+
+		template<class T>
+		T degrees(T radians) noexcept
+		{
+			return radians * static_cast<T>(180.0) / static_cast<float>(M_PI);
+		}
+
+		template<class T>
+		T radians(T degrees) noexcept
+		{
+			return degrees * static_cast<float>(M_PI) / static_cast<T>(180.0);
+		}
+
+		template<class T>
+		T length(const Vec2<T>& u) noexcept
+		{
+			return sqrt(pow(u.x, 2) + pow(u.y, 2));
+		}
+
+		template<class T>
+		T length_sq(const Vec2<T>& u) noexcept
+		{
+			return pow(u.x, 2) + pow(u.y, 2);
+		}
+
+		template<class T>
+		T dist_btwn(const Vec2<T>& u, const Vec2<T>& v) noexcept
+		{
+			return sqrt(pow(v.x - u.x, 2) + pow(v.y - u.y, 2));
+		}
+
+		template<class T>
+		T dist_btwn_sq(const Vec2<T>& u, const Vec2<T>& v) noexcept
+		{
+			return pow(v.x - u.x, 2) + pow(v.y - u.y, 2);
+		}
+
+		template<class T>
+		T angle_btwn(const Vec2<T>& u, const Vec2<T>& v) noexcept
+		{
+			return degrees(acos(dot(norm(u), norm(v))));
+		}
+
+		template<class T>
+		T dot(const Vec2<T>& u, const Vec2<T>& v) noexcept
+		{
+			return u.x * v.x + u.y * v.y;
+		}
+
+		template<class T>
+		Vec2<T> norm(const Vec2<T>& u) noexcept
+		{
+			auto mag = length(u);
+			if (mag == 0) throw std::runtime_error("Cannot normalize the zero vector");
+			return Vec2<T>(u.x / mag, u.y / mag);
+		}
+
+		template<class T>
+		Vec2<T> trunc(const Vec2<T>& u, T length) noexcept
+		{
+			return norm(u) * length;
+		}
+
+		template<class T>
+		Vec2<T> rotate(const Vec2<T>& u, T angle) noexcept
+		{
+			auto rads = radians(angle);
+			auto x = u.x * cos(rads) - u.y * sin(rads);
+			auto y = u.x * sin(rads) + u.y * cos(rads);
+			return Vec2<T>(x, y);
+		}
+
+		template<class T>
+		Vec2<T> ortho(const Vec2<T>& u) noexcept
+		{
 			return Vec2<T>(-u.y, u.x);
 		}
 
-		using Vec2f = Vec2<float>;
-		using Vec2d = Vec2<double>;
-		
-		// Matrix class specifically used for view transformations
+
+		// Matrix class for view transformations
 		template <class T>
-		class Mat3 {
+		class Mat2 {
 		public:
+			Mat2() : x11(0), x12(0), x21(0), x22(0) {}
+			Mat2(T x11, T x12, T x21, T x22)
+				: x11(x11), x12(x12), x21(x21), x22(x22) {}
 
-			// Returns the identity matrix
-			static Mat3<T> identity() noexcept;
+			static Mat2<T> identity() noexcept {
+				return Mat2<T>(1, 0, 0, 1);
+			}
 
-			// Returns a translation matrix
-			static Mat3<T> translation(const Vec2<T>& p) noexcept;
+			static Mat2<T> rotation(const Vec2<T>& u, const Vec2<T>& v) noexcept {
+				return Mat2<T>(u.x, u.y, v.x, v.y);
+			}
 
-			// Returns a rotation matrix
-			static Mat3<T> rotation(const Vec2<T>& u, const Vec2<T>& v) noexcept;
+			Vec2<T> operator*=(const Vec2<T>& p) noexcept {
+				auto x = p.x * x11 + p.y * x12;
+				auto y = p.x * x21 + p.y * x22;
+				return Vec2<T>(x, y);
+			}
 
 		private:
+			T x11, x12;
+			T x21, x22;
 		};
 
-		// Transforms a point from world space to local space
-		// Note:
-		// - xaxis and yaxis form the basis for the local space centered at origin
 		template <class T>
-		Vec2<T> point_to_local_space(const Vec2<T>& origin, const Vec2<T>& xaxis, const Vec2<T>& yaxis, const Vec2<T>& point) noexcept;
+		Vec2<T> operator*(const Mat2<T>& m, const Vec2<T>& p) noexcept { return Mat2<T>(m) *= p; }
 
-		// Transforms a point from local space to world space
-		// Note:
-		// - xaxis and yaxis form the basis for the local space centered at origin 
+		// Converts points in world space to entity local space
 		template <class T>
-		Vec2<T> point_to_world_space(const Vec2<T>& origin, const Vec2<T>& xaxis, const Vec2<T>& yaxis, const Vec2<T>& point) noexcept;
+		Vec2<T> convert_to_local_space(const Vec2<T>& origin, const Vec2<T>& u, const Vec2<T>& v, const Vec2<T>& p) noexcept {
+			Vec2<T> translated_point = p - origin;
+			Mat2<T> rotation(Mat2<T>::rotation(u, v));
+			return rotation * translated_point;
+		}
 
+		// Converts points in entity local space to world space
+		template <class T>
+		Vec2<T> convert_to_world_space(const Vec2<T>& origin, const Vec2<T>& u, const Vec2<T>& v, const Vec2<T>& p) noexcept {
+			Mat2<T> rotation(Mat2<T>::rotation(Vec2<T>(u.x, v.x), Vec2<T>(u.y, v.y)));
+			Vec2<T> rotated_point = rotation * p;
+			return rotated_point + origin;
+		}
+
+		// Spatial partitioner
+		// TODO: add event management
+		class CellSpace {
+		public:
+
+			using value_type = rift::Entity;
+			using size_type = std::vector<rift::Entity>::size_type;
+
+			// Boundary of a cell
+			struct Boundary {
+				Boundary() : x(0), y(0), w(0), h(0) {}
+				Boundary(size_type x, size_type y, size_type w, size_type h) 
+					: x(x), y(y), w(w), h(h) {}
+
+				// Checks for boundary intersection
+				bool intersects_with(const Boundary& other) const noexcept {
+					if (other.x >= x + w)
+						return false;
+					if (other.x + other.w <= x)
+						return false;
+					if (other.y >= y + h)
+						return false;
+					if (other.y + other.h <= y)
+						return false;
+					return true;
+				}
+
+				// Boundary components
+				size_type x, y, w, h;
+			};
+
+			// Contains entities in a boundary
+			struct Cell {
+				Cell(size_type x, size_type y, size_type w, size_type h)
+					: boundary(x, y, w, h) {}
+
+				// The boundaries of the cell
+				Boundary boundary;
+
+				// The entities in the cell's boundaries
+				std::list<rift::Entity> members;
+			};
+
+			CellSpace(size_type world_width, size_type world_height, size_type num_cells_x, size_type num_cells_y)
+				: world_width(world_width)
+				, world_height(world_height)
+				, num_cells_x(num_cells_x == 0 ? 1 : num_cells_x)
+				, num_cells_y(num_cells_y == 0 ? 1 : num_cells_y)
+			{
+				size_type cell_width = world_width / num_cells_x;
+				size_type cell_height = world_height / num_cells_y;
+
+				for (size_type x = 0; x < num_cells_x; x++) {
+					for (size_type y = 0; y < num_cells_y; y++) {
+						grid.push_back(Cell(x * cell_width, y * cell_height, cell_width, cell_height));
+					}
+				}
+			}
+
+			// Returns the number of managed entities
+			size_type size() const noexcept { std::accumulate(grid.begin(), grid.end(), size_type(0), [](size_type n, Cell cell) { return n + static_cast<size_type>(cell.members.size()); }); }
+
+			// Checks if an entity is contained in the spatial partitioning
+			bool contains(const rift::Entity& entity) const noexcept {
+				return entity_cell_locations.find(entity.id().number()) != entity_cell_locations.end();
+			}
+
+			// Returns the cell index for a given position
+			template <class T>
+			size_type cell_for(const Vec2<T>& p) noexcept {
+				// Formula -> column_length * column + row
+				size_type index = num_cells_y * static_cast<size_type>(num_cells_x * p.x / world_width)
+					              + static_cast<size_type>(num_cells_y * p.y / world_height);
+				return (index >= grid.size()) ? grid.size() - 1 : index;
+			}
+
+			// Adds a new entity into the appropriate cell space
+			// Note: 
+			// - Asserts the entity is not already a part of the spatial partitioning
+			template <class T>
+			void add_entity(const rift::Entity& entity, const Vec2<T>& p) noexcept {
+				assert(!contains(entity));
+				auto index = cell_for(p);
+				grid.at(index).members.push_back(entity);
+				entity_cell_locations.insert(std::make_pair(entity.id().number(), index));
+			}
+
+			// Removes an entity from the its associated cell space
+			// Note:
+			// - Asserts the entity is a part of the spatial partitioning
+			template <class T>
+			void remove(const rift::Entity& entity, const Vec2<T>& p) noexcept {
+				assert(contains(entity));
+				auto index = cell_for(p);
+				grid.at(index).members.remove(entity);
+				entity_cell_locations.erase(entity.id().number());
+			}
+
+			// Update the position of an entity
+			// Note:
+			// - Asserts the entity is a part of the spatial partitioning
+			template <class T>
+			void update_entity(const Entity& entity, const Vec2<T>& op, const Vec2<T>& p) noexcept {
+				assert(contains(entity));
+				auto old_cell = cell_for(op);
+				auto new_cell = cell_for(p);
+				if (old_cell != new_cell) {
+					grid.at(old_cell).members.remove(entity);
+					grid.at(new_cell).members.push_back(entity);
+					entity_cell_locations.at(entity.id().number()) = new_cell;
+				}
+			}
+
+
+		private:
+
+			// Grid of cells stored in column major form
+			std::vector<Cell> grid;
+
+			// Dimensions of the cell space
+			size_type world_width, world_height;
+
+			// Number of cells vertically and horizontally
+			size_type num_cells_x, num_cells_y;
+
+			// Address table from entities to cell locations
+			std::unordered_map<std::uint64_t, size_type> entity_cell_locations;
+		};
 	}
 
-	enum class BehaviorTypes {
+	namespace agent {
 
-	};
 
-	struct AutonomousAgent : Component<AutonomousAgent> {
+		enum class Behaviour {
 
-	};
+		};
 
-	struct AutonomousAgentSystem : public System<AutonomousAgentSystem> {
+		struct AutonomousAgent : public Component<AutonomousAgent> {
 
-	};
+		};
+
+		class AutonomousAgentSystem : public System <AutonomousAgentSystem> {
+		public:
+			AutonomousAgentSystem() = default;
+
+		private:
+			impl::CellSpace world;
+		};
+
+	}
 }
 
 #endif // !_RIFT_AUTONOMOUS_AGENT_SYSTEM_
