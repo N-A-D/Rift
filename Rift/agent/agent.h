b30c1ff52db5b26244182e23b208f04f88340665
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-11-04 17:04:10 -0500

    Added some behaviour types and the ability for users to select the floating point precision for the vectors used in calculations

--- Rift/agent/agent.h
+++ Rift/agent/agent.h
@@ -3,13 +3,36 @@
 #include "../system.h"
 #include "../component.h"
 
+#ifdef _USE_DOUBLE_PRECISION_FP_
+using _FP_PRECISION_TYPE_ = double;
+#else
+using _FP_PRECISION_TYPE_ = float;
+#endif // _USE_DOUBLE_PRECISION_FP_
+
+#include "details/vec2.h"
+#include "details/spatial.h"
+
 namespace rift {
 
+	enum class Behaviour {
+		NONE = 0x00000,
+		SEEK = 0x00001,
+		FLEE = 0x00002,
+	};
+
+	using Vec2D = details::Vec2<_FP_PRECISION_TYPE_>;
+	
 	struct AutonomousBody : public Component<AutonomousBody> {
+				
+		Vec2D pos;
+		Vec2D vel;
+		Vec2D dir;
 
 	};
 
-	struct AutonomousBodySystem : public System<AutonomousBodySystem> {
-
+	class AutonomousBodySystem : public System<AutonomousBodySystem> {
+	public:
+	private:
+		details::CellSpacePartition cell_space;
 	};
 }
