23fc2a828f0305ad94f7aa7d1bb0baaeb1e5714b
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-11-05 14:08:22 -0500

    Added some more behaviour types and renamed _USE_DOUBLE_PRECISION_FP_ to _AGENTS_USE_DOUBLE_PRECISION_FP_ to avoid name clashes

--- Rift/agent/agent.h
+++ Rift/agent/agent.h
@@ -2,25 +2,43 @@
 
 #include "../system.h"
 #include "../component.h"
+#include "../utility/event.h"
 
-#ifdef _USE_DOUBLE_PRECISION_FP_
-using _FP_PRECISION_TYPE_ = double;
+#ifdef _AGENTS_USE_DOUBLE_PRECISION_FP_
+using _AGENT_FP_PRECISION_TYPE_ = double;
 #else
-using _FP_PRECISION_TYPE_ = float;
-#endif // _USE_DOUBLE_PRECISION_FP_
+using _AGENT_FP_PRECISION_TYPE_ = float;
+#endif // AGENTS_USE_DOUBLE_PRECISION_FP_
 
 #include "details/vec2.h"
 #include "details/spatial.h"
 
 namespace rift {
 
-	enum class Behaviour {
-		NONE = 0x00000,
-		SEEK = 0x00001,
-		FLEE = 0x00002,
+	struct MovementEvent : public Event<MovementEvent> {
+		MovementEvent() : x(0), y(0) {}
+		MovementEvent(rift::Entity entity, _AGENT_FP_PRECISION_TYPE_ x, _AGENT_FP_PRECISION_TYPE_ y)
+			: x(x), y(y) {}
+		rift::Entity entity;
+		_AGENT_FP_PRECISION_TYPE_ x, y;
 	};
 
-	using Vec2D = details::Vec2<_FP_PRECISION_TYPE_>;
+	enum class Behaviour {
+		NONE       = 0x00000,
+		SEEK       = 0x00001,
+		FLEE       = 0x00002,
+		HIDE       = 0x00004,
+		EVADE      = 0x00008,
+		ARRIVE     = 0x00010,
+		WANDER     = 0x00020,
+		PURSUIT    = 0x00040,
+		INTERPOSE  = 0x00080,
+		COHESION   = 0x00100,
+		SEPARATION = 0x00200,
+		ALIGNMENT  = 0x00400,
+	};
+
+	using Vec2D = details::Vec2<_AGENT_FP_PRECISION_TYPE_>;
 	
 	struct AutonomousBody : public Component<AutonomousBody> {
 				
