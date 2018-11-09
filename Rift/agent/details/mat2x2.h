4f8b40925a7eddc7c6b79372e42e8ae65a04bf25
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-11-09 02:10:30 -0500

    Moved to agent namespace

--- Rift/agent/details/mat2x2.h
+++ Rift/agent/details/mat2x2.h
@@ -3,24 +3,25 @@
 #include "vec2.h"
 
 namespace rift {
-	namespace details {
+	namespace agent {
+		namespace details {
 
-		// 2x2 Matrix class used for point of view transformations
-		template <class T>
-		struct Mat2 {
-			Mat2(T x11, T x12, T x21, T x22)
-				: x11(x11), x12(x12), x21(x21), x22(x22) {}
+			// 2x2 Matrix class used for point of view transformations
+			template <class T>
+			struct Mat2 {
+				Mat2(T x11, T x12, T x21, T x22)
+					: x11(x11), x12(x12), x21(x21), x22(x22) {}
 
-			T x11, x12;
-			T x21, x22;
-		};
+				T x11, x12;
+				T x21, x22;
+			};
 
-		template <class T>
-		Vec2<T> operator+(const Mat2<T>& m, const Vec2<T>& u) noexcept {
-			auto x = u.x * m.x11 + u.y * m.x12;
-			auto y = u.x * m.x21 + u.y * m.x22;
-			return Vec2<T>(x, y);
+			template <class T>
+			Vec2<T> operator+(const Mat2<T>& m, const Vec2<T>& u) noexcept {
+				auto x = u.x * m.x11 + u.y * m.x12;
+				auto y = u.x * m.x21 + u.y * m.x22;
+				return Vec2<T>(x, y);
+			}
 		}
-
 	}
 }
