1a52a1353fd76938b9d66798c975d07b1db94b29
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-11-14 23:25:06 -0500

    Updated comment

--- Rift/utility/signature.h
+++ Rift/utility/signature.h
@@ -8,8 +8,8 @@ namespace rift {
 		// Given a template parameter pack of Component types, this function returns the ComponentMask for those types
 		// example: ComponentMask mask = signature_for<Position, Direction>();
 		// Note:
-		// - Ordering of the types does not matter, the function will still return the same component mask. That is, 
-		//   signature_for<Position, Direction>() == signature_for<Direction, Position>()
+		// - Ordering of the types does not matter, the function will still return the same component mask. That is, if
+		//   classes A and B are any two subclasses of rift::Component, signature_for<A, B>() == signature_for<B, A>()
 		template <class ...Components>
 		ComponentMask signature_for() noexcept {
 			static_assert(static_all_of<std::is_base_of<BaseComponent, Components>::value...>::value, 
