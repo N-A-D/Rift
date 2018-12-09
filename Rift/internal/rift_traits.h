458b2e7b37cd8b6ff8f2c41d8bc4ace58cd82bcb
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-09 13:59:52 -0500

    Revised static_all_of

--- Rift/internal/rift_traits.h
+++ Rift/internal/rift_traits.h
@@ -5,21 +5,15 @@
 namespace rift {
 	namespace impl {
 
-		// compile-time version of std::all_of
-
-		template <bool ...values> struct static_all_of;
-
-		template <bool ...tail>
-		struct static_all_of<true, tail...> : static_all_of<tail...> {};
-
-		template <bool... tail>
-		struct static_all_of<false, tail...> : std::false_type {};
-
-		template <> struct static_all_of<> : std::true_type {};
+		// Compile-time version of std::all_of 
+		// Taken from: https://stackoverflow.com/questions/27221443/implementing-static-version-of-stdall-of-using-template-metaprogramming
+		template <bool... values> struct bool_pack;
+		template <bool... values> struct all_of 
+			: std::is_same<bool_pack<values..., true>, bool_pack<true, values...>> {};
 
 		// Wrapper type to support lamba to std::function conversion
 		// Taken from: https://stackoverflow.com/questions/13358672/how-to-convert-a-lambda-to-an-stdfunction-using-templates
-		template <class T> struct Identity { using type = T; };
+		template <class T> struct identity { using type = T; };
 
 	}
 }
