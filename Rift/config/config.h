5c364e116c6a80bee873fded0622c020da15b550
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-10 19:36:36 -0500

    MAX_COMPONENT_TYPES is not constexpr

--- Rift/config/config.h
+++ Rift/config/config.h
@@ -6,7 +6,7 @@ namespace rift {
 	// Defines rift's configuration information
 	namespace config {
 		// If more component types are needed modify this value
-		static const std::size_t MAX_COMPONENT_TYPES = 128;
+		static constexpr std::size_t MAX_COMPONENT_TYPES = 128;
 	}
 	using ComponentMask = std::bitset<config::MAX_COMPONENT_TYPES>;
 }
