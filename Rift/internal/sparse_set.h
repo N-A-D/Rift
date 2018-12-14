13d9206f81443d2fe7c835972fbaee1dc5f17dbc
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-14 17:33:23 -0500

    Updated comment

--- Rift/internal/sparse_set.h
+++ Rift/internal/sparse_set.h
@@ -102,10 +102,10 @@ namespace rift {
 			// The number of integers in the set
 			value_type n = 0;
 
-			// Collection of integers
+			// Collection of integers in the set
 			std::vector<value_type> dense;
 
-			// Collection of integers present in the set
+			// Collection of nonnegative integers
 			std::vector<value_type> sparse;
 		};
 
