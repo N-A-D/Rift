98df54b523ac8427e6812265d69e2379fc604e43
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-13 20:38:08 -0500

    Removed redundant comments

--- Rift/config/config.h
+++ Rift/config/config.h
@@ -3,10 +3,11 @@
 #include <bitset>
 
 namespace rift {
-	// Defines rift's configuration information
+
 	namespace config {
-		// If more component types are needed modify this value
+
 		static const std::size_t MAX_COMPONENT_TYPES = 128;
+
 	}
 	using ComponentMask = std::bitset<config::MAX_COMPONENT_TYPES>;
 }
